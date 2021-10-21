e0676e6bb4003c8948f3d699ec90399ecaf9b0d6
Nicholas Stroble <nicholasstroble@nicholass-mbp.msdomain.indiancreekschool.com>
2021-10-21 12:39:13 -0400

    started research class and wrote idea for each method within that class

---
+++ ClickerGameProject/src/Research.java
@@ -0,0 +1,28 @@
+public class Research {
+
+    private double cpuModifierChange;
+    private int price;
+
+
+
+
+
+//set creativity
+//should set creativity to proper value if something is bougth 
+public void setCreativity(){
+}
+
+//set cpu modifier
+// this should be bought from store and then should change value/ multiplier of 
+// the cpu multiplier
+public void changecpuModifier(){
+}
+
+
+//set price 
+//if you buy something from the reasearch or store stores this should set the bits or creativity
+//to the proper price
+public void setprice(){
+}
+
+}
